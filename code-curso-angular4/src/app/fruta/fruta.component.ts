d1710c1ada409a7189c7f6d181cf7998209cdf63
Floren Blas <FlorenBlas@FlorenBlas.local>
2018-03-01 21:50:05 +0100

    prueba para subir

--- code-curso-angular4/src/app/fruta/fruta.component.ts
+++ code-curso-angular4/src/app/fruta/fruta.component.ts
@@ -8,4 +8,4 @@ import { Component } from "@angular/core";
 export class FrutaComponent {
     public nombre_componente:string = 'Componente de fruta';
     public listado_frutas:string = 'Naranja y Manzana';
-}   
+}    
