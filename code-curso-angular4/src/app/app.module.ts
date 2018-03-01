8f329a27e1f8fad3bec4cf41785995523ff32dff
Floren Blas <florenblas@gmail.com>
2018-03-01 17:58:38 +0100

    add files

--- code-curso-angular4/src/app/app.module.ts
+++ code-curso-angular4/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { FrutaComponent } from './fruta/fruta.component';
 import { OrdenadorComponent } from './ordenador/teclado.component';
 import { CarComponent } from './car/car.component';
+import { InstrumentComponent } from './instrument/instrument.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrutaComponent,
     OrdenadorComponent,
-    CarComponent
+    CarComponent,
+    InstrumentComponent
   ],
   imports: [
     BrowserModule,
--- code-curso-angular4/src/app/instrument/instrument.component.ts
+++ code-curso-angular4/src/app/instrument/instrument.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component ({
-    selector. 'instrument',
+    selector: 'instrument',
     templateUrl: '../instrument/instrument.component.html'
 })
 
-export class name {
-    constructor(parameters) {
-        
-    }
+export class InstrumentComponent {
+    public instrument1 = 'Instrumento 1';
+    public instrument2 = 'Instrumento 2';
 }
