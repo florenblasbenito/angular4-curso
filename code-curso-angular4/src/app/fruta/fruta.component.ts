3767a46d6817e64925c5d6affb67e4b4819c8ee6
Floren Blas <florenblas@gmail.com>
2018-03-02 15:23:49 +0100

    prueba components

--- code-curso-angular4/src/app/fruta/fruta.component.ts
+++ code-curso-angular4/src/app/fruta/fruta.component.ts
@@ -8,4 +8,32 @@ import { Component } from "@angular/core";
 export class FrutaComponent {
     public nombre_componente = 'Fruit of component';
     public listado_frutas = 'Orange and apple';
+
+    public nombre: string;
+    public edad: number;
+    public mayorDeEdad: boolean;
+    public trabajos: Array<any> = ['Carpintero', 44, 'Fontanero'];
+    commodin: any;
+   
+    constructor() {
+        this.nombre = 'holaaaa'
+        this.edad = 77
+        this.mayorDeEdad = false;
+        this.commodin = "SI";
+    }
+
+    ngOnInit() {
+        this.cambiarNombre();
+        this.cambiarEdad(45);
+        console.log(this.nombre + " " + this.edad);
+    }
+
+    cambiarNombre(){
+        this.nombre = 'Juan Lopez';
+    }
+
+    cambiarEdad(edad) {
+        this.edad = edad;
+    }
 }
+
